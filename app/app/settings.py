4a340ea7af35721aa80d1449ce6c730b6074bb1f
Treharne <treharne@liverpool.ac.uk>
2024-01-12 16:14:53 +0000

    tweak

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = True
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
